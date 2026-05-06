62ef0401a014ac4831869d8c0d35865e9853c8d9
feherszabolcs <feherszabi2005@gmail.com>
2026-05-06 11:14:27 +0200

    edited baseurl

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,19 +1,21 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:5148/api",
-    headers:{
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
+  baseURL: "http://192.168.1.69:5148/api",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 })
 
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData")!).token : null
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("userData")
+    ? JSON.parse(localStorage.getItem("userData")!).token
+    : null
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
 })
 
 export default api
