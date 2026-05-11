bae8075f9a52d82c48fc52caaeccc499836c9e19
feherszabolcs <feherszabi2005@gmail.com>
2026-05-11 21:10:31 +0200

    added register navigation functions

--- src/components/views/RegisterView.tsx
+++ src/components/views/RegisterView.tsx
@@ -1,11 +1,16 @@
 import { NavLink } from "react-router"
-import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
 import AssociationsComboBox from "../ui/associations"
 import { Controller, useForm } from "react-hook-form"
+import { Button } from "../ui/button"
+import { useState } from "react"
 
 const RegisterView = () => {
+    const PLANDER_ADMIN_URL = "http://localhost:4200"
+    const totalsteps = 2;
+    const [currentStep, setCurrentStep] = useState(1)
 
-    const { register, handleSubmit, control, formState: { errors } } = useForm({
+    const { register, handleSubmit, control, formState: { errors, isValid } } = useForm({
         defaultValues: {
             associationId: ""
         }
@@ -16,20 +21,33 @@ const RegisterView = () => {
             <CardHeader>
                 <CardTitle>Regisztráció</CardTitle>
                 <CardDescription>
-                    Válassza ki egyesületét.
+                    <p>Válassza ki egyesületét.</p> <br />
                 </CardDescription>
                 <CardAction>
                     <NavLink to="/auth/login" className='text-primary underline-offset-4 hover:underline'>Vissza a belépéshez</NavLink>
                 </CardAction>
             </CardHeader>
             <CardContent>
-                <Controller control={control} rules={{ required: "Az egyesület kiválasztása kötelező a regisztrációhoz!" }}
-                    name="associationId" render={({ field }) => (
-                        <AssociationsComboBox field={field} />
-                    )} />
+                {currentStep == 1 &&
+                    <>
+                        <Controller control={control} rules={{ required: "Az egyesület kiválasztása kötelező a regisztrációhoz!" }}
+                            name="associationId" render={({ field }) => (
+                                <AssociationsComboBox field={field} />
+                            )} />
+                        <p className="my-4 mx-auto">Nem találja egyesületét? Indítsa el a regisztrációs folyamatot
+                            <a href={PLANDER_ADMIN_URL} target="_blank" className='text-primary underline-offset-4 hover:underline'> ezen az oldalon.</a></p>
+                    </>
+                }
+
             </CardContent>
+            <CardFooter className="flex justify-between items-center">
+                <Button disabled={currentStep == 1}
+                    onClick={() => setCurrentStep(prev => prev - 1)}>← Vissza</Button>
+                <Button hidden={currentStep == totalsteps} disabled={!isValid}
+                    onClick={() => setCurrentStep(prev => prev + 1)}>Következő →</Button>
+                <Button hidden={currentStep != totalsteps}>✓ Regisztráció</Button>
+            </CardFooter>
         </Card>
     )
 }
-
 export default RegisterView
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://192.168.1.69:5148/api",
+  baseURL: "http://localhost:5148/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
