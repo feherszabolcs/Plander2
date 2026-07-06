05e4b94c5c8935c43158150449f120e80e2e0cea
feherszabolcs <feherszabi2005@gmail.com>
2026-07-06 11:34:39 +0200

    improved chats ui

--- src/components/views/Chats/ChatRoom.tsx
+++ src/components/views/Chats/ChatRoom.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Spinner } from "@/components/ui/spinner";
 import { useAuth } from "@/context/AuthContext";
 import { useAssociationChat } from "@/hooks/useAssociationChat";
-import { useState } from "react";
+import { Circle } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 
 
 function ChatRoom() {
@@ -9,36 +13,58 @@ function ChatRoom() {
     const associationId = user?.association?.id ?? null;
     const { status, messages, sendMessage } = useAssociationChat(token, associationId);
 
+    const chatContainerRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const container = chatContainerRef.current;
+        if (container) {
+            container.scrollIntoView(false);
+        }
+    }, [messages]);
+
     const handleSend = async () => {
         if (!draft.trim()) return;
         await sendMessage(draft.trim());
         setDraft("");
     };
 
+
     if (!associationId) {
-        return <div>Betöltés...</div>;
+        return <Spinner className="m-auto" />;
     } else
         return (
-            <div>
-                <h2>Chat egyesület: {user?.association.name ?? "nincs kiválasztva"}</h2>
-                <div>Kapcsolat: {status}</div>
+            <div className="flex flex-col h-full p-4">
+                <div className="flex justify-between items-center mb-4">
+                    <h1 className="text-2xl font-bold mb-4">Csevegés itt: <span className="font-bold italic">{user?.association.name ?? "nincs kiválasztva"}</span></h1>
+                    <div className="flex items-center gap-2" title={status == "connected" ? "Aktív" : (status == "disconnected" ? "Sikertelen csatlakozás" : "Csatlakozás folyamatban...")}>
+                        Kapcsolat: <span>
+                            <Circle fill={status == "connected" ? "green" : (status == "disconnected" ? "red" : "yellow")} stroke="none" />
+                        </span>
 
-                <div style={{ height: 300, overflowY: "auto", border: "1px solid #ccc", padding: 10 }}>
-                    {messages.map((msg, idx) => (
-                        <div key={idx}>
-                            <strong>{msg.sender}:</strong> {msg.message}
-                            <div style={{ fontSize: 12, color: "#666" }}>{new Date(msg.sentAt).toLocaleTimeString()}</div>
-                        </div>
-                    ))}
+                    </div>
                 </div>
 
-                <input
-                    value={draft}
-                    onChange={(e) => setDraft(e.target.value)}
-                    placeholder="Írd a üzeneted..."
-                    style={{ width: "80%" }}
-                />
-                <button onClick={handleSend}>Küldés</button>
+                <ScrollArea className=" max-h-96">
+                    <div ref={chatContainerRef} className="flex flex-col gap-2 overflow-y-auto border p-2 rounded">
+                        <ScrollBar />
+                        {messages.map((msg, idx) => (
+                            <div key={idx}>
+                                <strong>{msg.sender}:</strong> {msg.message}
+                                <div style={{ fontSize: 12, color: "#666" }}>{new Date(msg.sentAt).toLocaleTimeString()}</div>
+                            </div>
+                        ))}
+                    </div>
+                </ScrollArea>
+                <div className="flex mt-2">
+                    <input
+                        className="flex-1 border rounded px-2 py-1 mr-2"
+                        type="text"
+                        value={draft}
+                        onChange={(e) => setDraft(e.target.value)}
+                        placeholder="Írd a üzeneted..."
+                    />
+                    <Button className="" onClick={handleSend}>Küldés</Button>
+                </div>
             </div>
         );
 }
